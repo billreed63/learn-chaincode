a2b34fb85817314b53fd149e636b2db61020ecc0
Bill Reed <billreed@us.ibm.com>
2016-11-16 15:42:26 -0500

    test

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -88,7 +88,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 
 	key = args[0]
-	value = args[1]
+	value = args[1] + " xxxx"
 	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
